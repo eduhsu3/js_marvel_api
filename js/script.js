9b2e267e00ea6bbfe1c5606d69e1e0abde4b1a50
eduhsu3 <eduhsu3@gmail.com>
2024-08-16 13:53:25 +0900

    마블포기후 포켓몬으로 수정

--- js/script.js
+++ js/script.js
@@ -1,43 +1,71 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const timestamp = Date.now().toString();
-    const privateKey = '07525c8f8e047d827c303739ed5ac0075a508fd3';
-    const publicKey = 'c75233c772342459c41ec5c20e7070a0';
-
-    //해시생성
-    function generateHash(timestamp, privateKey, publicKey) {
-        const dataToHash = timestamp + privateKey + publicKey;
-        console.log(dataToHash);
-        return md5(dataToHash);
-    }
-
-    const hash = generateHash(timestamp, privateKey, publicKey);
-
-    console.log(hash);
+    //console.log(hash);
 
     //===========================================================
 
-    const baseUrl = 'http://gateway.marvel.com/v1/public/comics';
-    const queryString = `?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`;
+    const pokemonList = (limit = 10, offset = 0) => {
+        const baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+        const queryString = `/?limit=${limit}&offset=${offset}`;
+        fetch(baseUrl + queryString)
+            .then((res) => {
+                console.log(res);
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject('네트워크 장애');
+                }
+            })
+            .then((resData) => {
+                console.log(resData.results);
+                return resData.results;
+            })
+            .then((itemArr) => {
+                console.log(itemArr[0].url);
 
-    console.log(baseUrl + queryString);
-    fetch(baseUrl + queryString, {
-        method: 'GET',
-        headers: {
-            Accept: '*/*',
-        },
-    })
+                itemArr.map((item) => {
+                    fetch(item.url)
+                        .then((res) => {
+                            console.log(res);
+                            if (res.ok) {
+                                return res.json();
+                            } else {
+                                return Promise.reject('네트워크 장애');
+                            }
+                        })
+                        .then((resData) => {
+                            console.log('최종 : ', resData.results);
+                            // const final = resData.map((item) => {
+                            //     return {
+                            //         id: item.id,
+                            //         name: item.name,
+                            //         image: data.sprites.other['official-artwork'].front_default,
+                            //     };
+                            // });
+                            //console.log(final);
+                            //return final;
+                        })
+                        .catch(() => {});
+                    //return resData.results;
+                });
+            })
+            .catch(() => {});
+    };
+    const listData = pokemonList(20);
+
+    /* 
+    fetch(baseUrl)
         .then((res) => {
-            console.log(res);
+            //console.log(res);
             if (res.ok) {
                 return res.json();
             } else {
                 return Promise.reject('네트워크 장애');
             }
         })
-        .then((data) => {
-            console.log(data);
+        .then((resData) => {
+            console.log(resData);
         })
         .catch((err) => {
             console.error('에러내용 : ', err);
-        });
+        }); */
 });
